c546b2024bd639e91f1c64c1e0c95f0483f7441a
Geoffrey Leclaire <geoffrey.leclaire@polymtl.ca>
2017-11-29 16:52:06 -0500

    Favicon fix

--- TP3/MusicSearchService/app.js
+++ TP3/MusicSearchService/app.js
@@ -4,8 +4,11 @@ var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var session = require("express-session");
+var favicon = require("serve-favicon");
 
 var app = express();
+
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use(logger("dev"));
 
