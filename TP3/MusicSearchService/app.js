8c203b6438745db5c51858d5411217d08888c71e
Geoffrey Leclaire <geoffrey.leclaire@polymtl.ca>
2017-12-03 17:35:42 -0500

    Cleanup

--- TP3/MusicSearchService/app.js
+++ TP3/MusicSearchService/app.js
@@ -3,12 +3,8 @@ var path = require("path");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
-var session = require("express-session");
-var favicon = require("serve-favicon");
 
 var app = express();
-
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use(logger("dev"));
 
@@ -16,14 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-// initialize the session
-app.use(session({
-  secret: 'log4420',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
 
 app.use("/search", require("./routes/search"));
 
